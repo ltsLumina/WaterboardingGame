416c0bd7c0f527cb1b16053388f9a04ee15e73ee
Kudowastaken <128066867+Kudowastaken@users.noreply.github.com>
2023-05-12 11:08:35 +0200

    Revert "Alex branch v2"

--- 3D Platformer/Assets/Scripts/PlayerController.cs
+++ 3D Platformer/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 #region
-using System;
 using System.Collections;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -44,19 +43,21 @@ public class PlayerController : MonoBehaviour
     float coyoteTimeCounter;
     float currentSpeed;
     float fallingTimer;
+    public float FallingTimer { get => fallingTimer; private set => fallingTimer = value; }
     bool isGrounded;
+    public bool IsGrounded { get => isGrounded; private set => isGrounded = value; }
+
     bool isJumping;
+    public bool IsJumping { get => isJumping; private set => isJumping = value; }
+
     bool landingLock;
+    public bool LandingLock { get => landingLock; private set => landingLock = value; }
     public bool cursorLock;
     public static bool gameIsPaused;
     Transform mainCamera;
     Vector2 movementInput;
 
     bool isDead;
-
-    // onDeath event
-    public delegate void OnDeath();
-    public event OnDeath onDeath;
 
     public bool IsDashing { get; set; }
 
@@ -74,7 +75,7 @@ public class PlayerController : MonoBehaviour
 
             if (!isDead) return;
             Debug.Log("Player Died!");
-            onDeath?.Invoke();
+            // TODO: death
         }
     }
 
@@ -88,8 +89,6 @@ public class PlayerController : MonoBehaviour
         MyRigidbody = GetComponent<Rigidbody>();
         if (Camera.main != null) mainCamera = Camera.main.transform;
         characterAnimator = GetComponentInChildren<Animator>();
-
-        onDeath += DoPlayerDeath;
     }
 
     void OnPause()
@@ -299,14 +298,4 @@ public class PlayerController : MonoBehaviour
         IsDashing = false;
     }
     #endregion
-
-    void DoPlayerDeath() => StartCoroutine(HandlePlayerDeath());
-
-    IEnumerator HandlePlayerDeath()
-    {
-        yield return new WaitForSeconds(1f);
-        SceneManagerExtended.ReloadScene();
-    }
-
-    void OnDestroy() => onDeath -= DoPlayerDeath;
 }
